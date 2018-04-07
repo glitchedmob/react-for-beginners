3271eafae0b4d53cfc07278224388c322c249d67
Levi Zitting <levizitting5663@gmail.com>
2018-04-07 16:16:08 -0500

    Order basics and syncing with firebase

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,30 @@
 import React from 'react';
 
+import base from '../base'
 import { Header } from './Header';
 import { Inventory } from './Inventory';
 import { Order } from './Order';
+import { Fish } from './Fish';
+
+import sampleFishes from '../sample-fishes';
 
 
 export class App extends React.Component {
 	state = {
 		fishes: {},
 		order: {}
+	}
+
+	componentDidMount() {
+		const { params } = this.props.match;
+		this.ref = base.syncState(`${params.storeId}/fishes`, {
+			context: this,
+			state: 'fishes'
+		})
+	}
+
+	componentWillUnmount() {
+		base.removeBinding(this.ref);
 	}
 
 	addFish = fish => {
@@ -19,14 +35,36 @@ export class App extends React.Component {
 		this.setState({	fishes });
 	}
 
+	loadSampleFishes = () => {
+		this.setState({ fishes: sampleFishes })
+	}
+
+	addToOrder = key => {
+		const order = { ...this.state.order }
+
+		order[key] = order[key] + 1 || 1
+
+		this.setState({ order })
+	}
+
 	render() {
 		return (
 			<div className="catch-of-the-day">
 				<div className="menu">
 					<Header tagline="Wes is cool"/>
+					<ul className="fishes">
+						{ Object.keys(this.state.fishes).map(key => (
+							<Fish
+								key={ key }
+								index={ key }
+								fish={ this.state.fishes[key] }
+								addToOrder={ this.addToOrder }
+							/>
+						))}
+					</ul>
 				</div>
-				<Order />
-				<Inventory addFish={ this.addFish } />
+				<Order fishes={ this.state.fishes } order={ this.state.order }/>
+				<Inventory addFish={ this.addFish } loadSampleFishes= { this.loadSampleFishes } />
 			</div>
 		)
 	}
